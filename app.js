093cc2719b108c2062c5cb6fda4b7d48bcbbfb87
Spritix <duval.kelian@gmail.com>
2015-12-04 00:52:29 +0100

    Recuperation de variable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var lat = 41.878114;
+var longi = -87.629798;
+var rayon = 2000;
 
 app.get('/', function(req, res) {
-	res.render('index.ejs');
+	res.render('index.ejs',{lat:lat, longi:longi , rayon :rayon });
 });
 
 app.get('/connexion', function(req, res) {
