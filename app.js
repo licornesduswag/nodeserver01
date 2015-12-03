5661ecbfa4d63b5a99af65356b603b21b8234e8e
Paul-Etienne <pauletienne.francois@gmail.com>
2015-12-04 00:08:34 +0100

    Ajout de la gestion du 404

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 var express = require('express');
-
-
 var app = express();
 
-
-
 app.get('/', function(req, res) {
-
-
 	res.render('index.ejs');
-
-
 });
 
 app.get('/connexion', function(req, res) {
-
-
 	res.render('connexion.ejs');
-
-
 });
 
 app.get('/inscription', function(req, res) {
-
-
 	res.render('inscription.ejs');
+});
 
-
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable.');
 });
 
 app.listen(8080);
