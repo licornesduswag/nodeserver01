120fa62f62e39bd4b8ca26f49e29455c9c3a3cf0
Paul-Etienne <pauletienne.francois@gmail.com>
2015-12-04 01:18:54 +0100

    Ajout de l'inscription

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+}));
 
 app.get('/', function(req, res) {
 	res.render('index.ejs');
@@ -10,7 +17,34 @@ app.get('/connexion', function(req, res) {
 });
 
 app.get('/inscription', function(req, res) {
-	res.render('inscription.ejs');
+	res.render('inscription.ejs', { erreur : '' });
+});
+
+app.post('/do_inscription', function(req, res) {
+	if (req.body.pass == req.body.pass_confirm) {
+		mongoose.connect('mongodb://localhost/unisafe', function(err) {
+			if (err) { throw err; }
+		});
+
+		var userSchema = new mongoose.Schema({
+			login : String,
+			password : String
+		});
+
+		var userModel = mongoose.model('users', userSchema);
+
+		var newUser = new userModel({ login : req.body.login, password : req.body.pass });
+
+		newUser.save(function (err) {
+			if (err) { throw err; }
+			mongoose.connection.close();
+		});
+
+		res.render('index.ejs');
+	}
+	else {
+		res.render('inscription.ejs', { erreur : 'Erreur : Les mots de passe ne correspondent pas.' });
+	}
 });
 
 app.use(function(req, res, next){
